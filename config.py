2c5f64192396e5814c5a0c65575a6d1df2631c64
Chak10 <francescosorb@live.it>
2024-10-27 19:18:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Percorsi per ffmpeg e ffprobe
-FFMPEG_PATH = "D:\\Download\\VideoDuplicatorV2\\ffmpeg.exe"  # Sostituisci con il percorso completo se necessario
-FFPROBE_PATH = "D:\\Download\\VideoDuplicatorV2\\ffprobe.exe"  # Sostituisci con il percorso completo se necessario
+FFMPEG_PATH = "ffmpeg.exe"  # Sostituisci con il percorso completo se necessario
+FFPROBE_PATH = "ffprobe.exe"  # Sostituisci con il percorso completo se necessario
 FRAMES_DIR = "frames"  # Directory dove verranno salvati i frame estratti
 # Configurazione del database
 DB_FILE = "video_data.db"  # Nome del file del database SQLite
